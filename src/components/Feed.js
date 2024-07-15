c3a73655b294e58762adc2ab652edf5438ef068f
Saifullah Amin <saifullahaminkhan@gmail.com>
2024-07-16 00:18:11 +0500

    first commit

---
+++ src/components/Feed.js
@@ -0,0 +1,15 @@
+import Post from './Post';
+import { useStoreState } from 'easy-peasy';
+
+const Feed = () => {
+  const searchResults = useStoreState((state) => state.searchResults);
+  return (
+    <>
+      {searchResults.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default Feed;
